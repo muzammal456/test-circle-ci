976d168fc9300a64801bd44b66312ca34eb6b21e
muzammal456 <muzammalikram781@gmail.com>
2019-12-24 17:21:28 +0500

    sdsd

--- pull.php
+++ pull.php
@@ -2,7 +2,9 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-    exec("cd /var/www/html/test-circle-ci && git pull origin master");
+
+    echo shell_exec("git pull origin master");
+
     echo "deploy";
 
 
