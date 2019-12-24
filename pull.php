357ffff85fc0c342f8d9433a218eee7376cfbdcc
muzammal456 <muzammalikram781@gmail.com>
2019-12-24 17:05:06 +0500

    change

--- pull.php
+++ pull.php
@@ -2,8 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
-    shell_exec( `cd home/lqur9smlfhie/public_html/deploy/ && git reset –hard HEAD && git pull` );
-}
+    shell_exec( `git pull origin master` );
+
 
 ?>
